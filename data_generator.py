c9ba94c7f39552d77186bf39c2fcce0968483deb
Viacheslav Meshchaninov <meshchaninov.olymp@yandex.ru>
2023-08-03 12:17:10 +0300

    working version

---
+++ data_generator.py
@@ -0,0 +1,74 @@
+import torch
+from torchvision.datasets import MNIST
+from torchvision.transforms import (
+    Resize,
+    Normalize,
+    Compose,
+    ToTensor
+)
+from torch.utils.data import DataLoader
+import os
+from PIL import Image
+
+
+class DataGenerator:
+    def __init__(self, config):
+        self.config = config
+        self.mnist_transforms = Compose(
+            [
+                Resize((config.data.image_size, config.data.image_size)),
+                ToTensor(),
+                Normalize(mean=config.data.norm_mean, std=config.data.norm_std),
+                # to [-1; 1]
+            ]
+        )
+        self.train_loader = DataLoader(
+            MNIST(root='./data', download=True, train=True, transform=self.mnist_transforms),
+            batch_size=config.training.batch_size,
+            shuffle=True,
+            drop_last=True
+        )
+        self.valid_loader = DataLoader(
+            MNIST(root='./data', download=True, train=False, transform=self.mnist_transforms),
+            batch_size=5 * config.training.batch_size,
+            shuffle=False,
+            drop_last=True
+        )
+
+    def sample_train(self):
+        while True:
+            for batch in self.train_loader:
+                yield batch
+
+    def sample_val(self):
+        while True:
+            for batch in self.valid_loader:
+                yield batch
+
+
+class CustomDataGenerator(torch.utils.data.Dataset):
+
+    def __init__(self, config, path):
+        self.transform = Compose(
+            [
+                Resize((config.data.image_size, config.data.image_size)),
+                ToTensor(),
+                Normalize(mean=config.data.norm_mean, std=config.data.norm_std),
+                # to [-1; 1]
+            ]
+        )
+        self.path = path
+        self.all_images = os.listdir(self.path)
+
+    def __getitem__(self, idx):
+        image_path = self.path + '/' + self.all_images[idx]
+        img = Image.open(image_path)
+        target = int(image_path.split('class_')[1].split('.')[0])
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.all_images)
---
+++ trainer.py
@@ -0,0 +1,7 @@
+from config import create_default_mnist_config
+from diffusion import DiffusionRunner
+
+config = create_default_mnist_config()
+diffusion = DiffusionRunner(config)
+
+diffusion.train()
