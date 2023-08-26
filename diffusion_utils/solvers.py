57ec5e61d7d4462c38245a32055f427fde22ec70
Viacheslav Meshchaninov <meshchaninov.olymp@yandex.ru>
2023-08-26 09:52:06 +0300

    add ddpm sampling, ffhq v1

--- diffusion_utils/solvers.py
+++ diffusion_utils/solvers.py
@@ -7,6 +7,8 @@ def create_solver(config):
         return EulerDiffEqSolver
     elif config.dynamic.solver == "ddim":
         return DDIMSolver
+    elif config.dynamic.solver == "ddpm":
+        return DDPMSolver
 
 
 class EulerDiffEqSolver:
@@ -53,6 +55,42 @@ class DDIMSolver:
         mu = torch.sqrt(alpha_t_1) * x_0 + \
              torch.sqrt(1 - alpha_t_1 - sigma_t ** 2) * noise_t
         std = sigma_t
+        return mu, std
+
+    def step(self, x_t, t, labels=None):
+        """
+        Implement reverse SDE/ODE Euler solver
+        """
+
+        """
+        x_mean = deterministic part
+        x = x_mean + noise (yet another noise sampling)
+        """
+        noise = torch.randn_like(x_t)
+        x_0 = self.score_fn(x_t, t)["x_0"]
+        mu, std = self.q_x_t_reverse(x_t, x_0, t)
+        x = mu + std * noise
+        return {
+            "x": x,
+            "x_mean": mu
+        }
+
+
+class DDPMSolver:
+    def __init__(self, dynamic, score_fn, ode_sampling=False):
+        self.dynamic = dynamic
+        self.score_fn = score_fn
+        self.ode_sampling = ode_sampling
+
+    def q_x_t_reverse(self, x_t, x_0, t):
+        dt = 1 / self.dynamic.N
+        alpha_t = self.dynamic.marginal_params(t)["mu"] ** 2
+        alpha_t_1 = self.dynamic.marginal_params(t - dt)["mu"] ** 2
+        beta_t = 1 - alpha_t / alpha_t_1
+
+        mu = torch.sqrt(alpha_t_1) * beta_t / (1 - alpha_t) * x_0 + \
+             torch.sqrt(1 - beta_t) * (1 - alpha_t_1) / (1 - alpha_t) * x_t
+        std = torch.sqrt((1 - alpha_t_1) / (1 - alpha_t) * beta_t)
         return mu, std
 
     def step(self, x_t, t, labels=None):
